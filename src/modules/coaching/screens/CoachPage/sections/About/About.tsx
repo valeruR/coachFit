be066e86466645d5e4a2a10fb37bf0b18a25e6b1
reever1 <reever@getjoe.co>
2023-05-25 22:30:39 +0200

    feat to add description in about section in CoachPage

--- src/modules/coaching/screens/CoachPage/sections/About/About.tsx
+++ src/modules/coaching/screens/CoachPage/sections/About/About.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { RootState, useAppSelector } from '../../../../../../redux';
 
 const About = () => {
+  const coach = useAppSelector((state: RootState) => state.coach);
+
   return (
-    <View>
-      <Text>About</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>About</Text>
+      <Text style={styles.subtitle}>A BRIEF INTRO</Text>
+      <Text style={styles.description}>{coach.description}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: 'grey',
+    fontSize: 10,
+  },
+  description: {
+    marginTop: 10,
+  },
+});
 
 export default About;
